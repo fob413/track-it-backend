e92ac57ab700ca77624d482fd268ec8afef1238a
fob1493 <fob1493@gmail.com>
2018-07-21 01:15:41 +0100

    chore(deploy): heroku
    - deploy application to heroku

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
-from api import create_app, db
+from api import app, db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from api.models import Users
 
 
-app = create_app()
 migrate = Migrate(app, db)
 manager = Manager(app)
 
