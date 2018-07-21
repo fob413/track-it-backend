4a613f641dd1dd10d8a78b0fab63c2014e3a9dfb
fob1493 <fob1493@gmail.com>
2018-07-21 18:46:16 +0100

    updated work

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,3 +1,6 @@
 from .users import Users
 from .shipments import Shipments
 from .pfi import Pfi
+from .formm import Formm
+from .insurance import Insurance
+from .letterofcredit import LetterOfCredit
