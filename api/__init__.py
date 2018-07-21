504a125be758d401e7e205cdaf553deedfbcefba
Celoka <eloka.chima@gmail.com>
2018-07-21 02:10:57 +0100

    update

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,13 @@
 import os
 
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 from api.views.dummy_api import Dummy
 
 db = SQLAlchemy()
-
+login_manager = LoginManager()
 
 app = Flask(__name__)
 
@@ -14,13 +15,11 @@ app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 
 db.init_app(app)
+login_manager.init_app(app)
+login_manager.login_message = "You must be logged in to access this page."
 
-<<<<<<< HEAD
-api = Api(app)
-=======
 from api.views.manage_user import login_blueprint, logout_blueprint
 app.register_blueprint(login_blueprint)
 app.register_blueprint(logout_blueprint)
->>>>>>> login deployment
 
 app.shell_context_processor({'app': app, 'db': db})
